f0fe502ef6d9bdd6d62649dc3077826137c3fc05
Craige McWhirter <craige@mcwhirter.io>
2019-09-27 07:43:52 +1000

    Added an example of connecting to a hidden network

--- networking/wireless.nix
+++ networking/wireless.nix
@@ -18,7 +18,11 @@
       "example's SSID" = {        # Connect to an SSID with spaces and punctuation characters
         pskRaw = "46c25aa68ccb90945621c1f1adbe93683f884f5f31c6e2d524eb6b446642762d";
       };
+      myHiddenSSID = {            # An example of how to connect to a hidden SSID.
+        hidden = true;
+        pskRaw = "46c25aa68ccb90945621c1f1adbe93683f884f5f31c6e2d524eb6b446642762d";
       FreeWiFi = {};              # An example of how to connect to an open SSID.
+      };
     };
   };
 
